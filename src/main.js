e6148527b2c44ee286a509c08d7bfe3a6052dc39
pratiksonawane80 <pratiksonawane80@gmail.com>
2021-09-23 20:52:56 +0530

    Initial Commit

---
+++ src/main.js
@@ -0,0 +1,45 @@
+import { createApp } from 'vue';
+import { createRouter, createWebHistory } from 'vue-router';
+
+import App from './App.vue';
+import TeamsList from './components/teams/TeamsList.vue';
+import UsersList from './components/users/UsersList.vue';
+import TeamMembers from './components/teams/TeamMembers.vue';
+import NotFound from './components/nav/NotFound.vue';
+import UserFooter from './components/users/UserFooter.vue';
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: '/', redirect: '/teams' },
+    {
+      name: 'teams',
+      path: '/teams',
+      components: { default: TeamsList, footer: UserFooter },
+      children: [
+        {
+          name: 'team-members',
+          path: ':teamId',
+          component: TeamMembers,
+          props: true
+        } // /teams/t1
+      ]
+    }, // our-domain.com/teams => TeamsList
+    { path: '/users', components: { default: UsersList, footer: UserFooter } },
+
+    { path: '/:notFound(.*)', component: NotFound }
+  ],
+  linkActiveClass: 'active',
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition;
+    }
+    return { left: 0, top: 0 };
+  }
+});
+
+const app = createApp(App);
+
+app.use(router);
+
+app.mount('#app');
